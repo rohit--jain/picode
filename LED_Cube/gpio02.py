f9f09536db4c05386ac04c8b4030be13cf5a3141
Rohit Jain <rohit.jain.324@gmail.com>
2021-06-27 23:53:38 +0200

    bug fixes

--- LED_Cube/gpio02.py
+++ LED_Cube/gpio02.py
@@ -2,14 +2,19 @@
 import RPi.GPIO as GPIO
 
 def main():
-    pin_value= input("Please Enter The Pin Number:")
-    pin_number = int(pin_value)
+    grid_value= input("Please Enter The Pin Number: \n[7,11,35,37,12,13,31,33,15,16,23,29,18,19,21,22]:")
+    layer_value= input("Please Enter The Layer Number [40,38,36,32]: ")
+    grid_number = int(grid_value)
+    layer_number = int(layer_value)
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(pin_number, GPIO.OUT)
-    GPIO.output(pin_number,1)
+    GPIO.setup(grid_number, GPIO.OUT)
+    GPIO.setup(layer_number, GPIO.OUT)
+    GPIO.output(layer_number,1)
+    GPIO.output(grid_number,1)
     key_value = input("press space & enter to finish...")
-    GPIO.output(pin_number,0)
+    GPIO.output(grid_number,0)
+    GPIO.output(layer_number,0)
     GPIO.cleanup()
     print("Done")
 
