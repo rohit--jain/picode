5d1747633bd6582d82d7beb0c5a6f430d37baf78
Rohit Jain <rohit.jain.324@gmail.com>
2021-06-27 23:37:00 +0200

    bug fixes

--- LED_Cube/gpio02.py
+++ LED_Cube/gpio02.py
@@ -3,12 +3,13 @@ import RPi.GPIO as GPIO
 from time import sleep
 
 def main():
-    pin_number= input("Please Enter The Pin Number:")
+    pin_value= input("Please Enter The Pin Number:")
+    pin_number = int(pin_value)
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(pin_number, GPIO.OUT)
     GPIO.output(pin_number,True)
-    key = input("press enter to finish...")
+    key_value = input("press space & enter to finish...")
     GPIO.output(pin_number,False)
     GPIO.cleanup()
     print("Done")
