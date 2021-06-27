2818014f55f28e403e66ee55612e4c7c6dc18f4b
Rohit Jain <rohit.jain.324@gmail.com>
2021-06-27 17:55:25 +0200

    Initial Commit

---
+++ LED_Cube/gpio01.py
@@ -0,0 +1,9 @@
+# First GPIO Programming Guide for GPIO
+
+from gpiozero import LED
+from time import sleep
+
+def main():
+    print("Hello My First GPIO Programming Code")
+
+if __name__ == "__main__":main()
