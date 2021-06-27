514e00166c812bba02ee3fc77cd79c8a3d7496c1
Rohit Jain <rohit.jain.324@gmail.com>
2021-06-27 19:31:00 +0200

    Initial Commit

--- LED_Cube/gpio01.py
+++ LED_Cube/gpio01.py
@@ -6,6 +6,9 @@ GRID = [7,11,35,37,12,13,31,33,15,16,23,29,18,19,21,22]
 LAYER = [40,38,36,32]
 
 def main():
+    
+    GPIO.setmode(GPIO.BOARD)
+
     key = input("Testing Each LED. Press Any Key To Continue...")
     for i in range(0,15):
         GPIO.output(GRID[i],True)
@@ -13,6 +16,7 @@ def main():
         print("Starting LED on Pin Number: "+ str(GRID[i]))
         GPIO.output(GRID[i],False)
         sleep(1)
+    
     key = input("Now testing Layers. Press Any Key To Continue...")
     for i in range (0,3):
         GPIO.output(LAYER[i],True)
