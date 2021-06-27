3a8c390459a6c0e87bf12aa03ae487f00e2fd6ef
Rohit Jain <rohit.jain.324@gmail.com>
2021-06-27 19:25:47 +0200

    First LED Cube test program

--- LED_Cube/gpio01.py
+++ LED_Cube/gpio01.py
@@ -1,6 +1,5 @@
 # First GPIO Programming Guide for GPIO
-
-from gpiozero import LED
+import RPi.GPIO as GPIO
 from time import sleep
 
 GRID = [7,11,35,37,12,13,31,33,15,16,23,29,18,19,21,22]
@@ -9,19 +8,17 @@ LAYER = [40,38,36,32]
 def main():
     key = input("Testing Each LED. Press Any Key To Continue...")
     for i in range(0,15):
-        led = LED(GRID[i])
-        led.on()
+        GPIO.output(GRID[i],True)
         sleep(1)
         print("Starting LED on Pin Number: "+ str(GRID[i]))
-        led.off()
+        GPIO.output(GRID[i],False)
         sleep(1)
     key = input("Now testing Layers. Press Any Key To Continue...")
     for i in range (0,3):
-        led = LED(LAYER[i])
-        led.on()
+        GPIO.output(LAYER[i],True)
         sleep(2)
         print("Starting LAYER on Pin Number: "+ str(LAYER[i]))
-        led.off()
+        GPIO.output(LAYER[i],False)
         sleep(1)
     print("Testing Complete")
 
